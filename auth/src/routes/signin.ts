e3bc884b6887592cb96ad4adaa9c308fa36b44ad
Joris Sparla <joris.sparla@gmail.com>
2020-04-27 18:27:59 +0200

    auth flow and mongoose

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,9 +1,33 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import { body, validationResult } from "express-validator";
+import jwt from "jsonwebtoken";
+import { User } from "../models/user";
+import { validateRequest } from "../middlewares/validate-request";
+import { BadRequestError } from "../errors";
+import { Password } from "../services/password";
 
 const router = express.Router();
 
-router.post("/api/users/signin", (req, res) => {
-  res.send("Hello there");
-});
+router.post(
+  "/api/users/signin",
+  [body("email").isEmail().withMessage("Email must be valid"), body("password").trim().notEmpty().withMessage("Password must not be empty")],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+    const existingUser = await User.findOne({ email });
+    if (!existingUser) {
+      throw new BadRequestError("Login Request Failed");
+    }
+    const passwordsMatch = await Password.compare(existingUser.password, password);
+    if (!passwordsMatch) {
+      throw new BadRequestError("Invalid credentials");
+    }
+
+    const userJWT = jwt.sign({ id: existingUser.id, email: existingUser.email }, process.env.JWT_KEY!);
+
+    req.session = { jwt: userJWT };
+    res.status(200).send(existingUser);
+  }
+);
 
 export { router as signinRouter };
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,24 +1,41 @@
 import express, { Request, Response } from "express";
-import { body, validationResult } from "express-validator";
-import { DatabaseConnectionError, RequestValidationError } from "../errors";
+import { body } from "express-validator";
+import jwt from "jsonwebtoken";
+import { BadRequestError } from "../errors";
+import { validateRequest } from "../middlewares/validate-request";
+import { User } from "../models/user";
 
 const router = express.Router();
 
 router.post(
   "/api/users/signup",
   [
-    body("email").isEmail().withMessage("Email invalid"),
+    body("email").isEmail().withMessage("Email must be valid"),
     body("password").trim().isLength({ min: 4, max: 20 }).withMessage("Password must be between 4 and 20 characters"),
   ],
-  (req: Request, res: Response) => {
-    const errors = validationResult(req);
+  validateRequest,
+  async (req: Request, res: Response) => {
+    // const errors = validationResult(req);
+
+    // if (!errors.isEmpty()) {
+    //   throw new RequestValidationError(errors.array());
+    // }
+
     const { email, password } = req.body;
-    if (!errors.isEmpty()) {
-      throw new RequestValidationError(errors.array());
+
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      console.log("Email in use");
+      throw new BadRequestError("Email already in use");
     }
-    throw new DatabaseConnectionError();
-    console.log("Creating a user");
-    res.send("Hello there");
+
+    const user = User.build({ email, password });
+    await user.save();
+    const userJWT = jwt.sign({ id: user.id, email: user.email }, process.env.JWT_KEY!);
+
+    req.session = { jwt: userJWT };
+    res.status(201).send(user);
   }
 );
 
