042fab738003576a51c64c83432600f520462a5f
Joris Sparla <joris.sparla@gmail.com>
2020-04-27 15:07:06 +0200

    initial commit

---
+++ auth/src/routes/signup.ts
@@ -0,0 +1,25 @@
+import express, { Request, Response } from "express";
+import { body, validationResult } from "express-validator";
+import { DatabaseConnectionError, RequestValidationError } from "../errors";
+
+const router = express.Router();
+
+router.post(
+  "/api/users/signup",
+  [
+    body("email").isEmail().withMessage("Email invalid"),
+    body("password").trim().isLength({ min: 4, max: 20 }).withMessage("Password must be between 4 and 20 characters"),
+  ],
+  (req: Request, res: Response) => {
+    const errors = validationResult(req);
+    const { email, password } = req.body;
+    if (!errors.isEmpty()) {
+      throw new RequestValidationError(errors.array());
+    }
+    throw new DatabaseConnectionError();
+    console.log("Creating a user");
+    res.send("Hello there");
+  }
+);
+
+export { router as signupRouter };
