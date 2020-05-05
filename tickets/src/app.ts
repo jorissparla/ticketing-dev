f0377b15fd8a27647fbda717e6759c8719264c0e
Joris Sparla <joris.sparla@gmail.com>
2020-05-05 13:22:44 +0200

    finished tickets

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -7,6 +7,8 @@ import cookieSession from "cookie-session";
 import { errorHandler, NotFoundError, currentUser, requireAuth } from "@js-tickets/common";
 import { createTicketRouter } from "./routes/new";
 import { showTicketRouter } from "./routes/show";
+import { indexTicketRouter } from "./routes/index";
+import { updateTicketRouter } from "./routes/update";
 
 const app = express();
 app.set("trust proxy", true);
@@ -21,6 +23,8 @@ app.use(
 app.use(currentUser);
 app.use(createTicketRouter);
 app.use(showTicketRouter);
+app.use(indexTicketRouter);
+app.use(updateTicketRouter);
 app.all("*", async (req, res) => {
   throw new NotFoundError();
 });
